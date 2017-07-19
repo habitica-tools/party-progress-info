4486eb0dfd4820c1586e8e18a35c539bc24fd2dd
pieter vanh <pieter.vanh@gmail.com>
2017-07-19 20:41:07 +0200

    Add Footer + Surge deploy option

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
   render({store}) {
     return (
+      <div>
       <div class="ui main container">
         <h2 class="ui dividing header">
           Habitica Party Tools
@@ -26,6 +27,22 @@ class App extends Component {
             <QuestList store={store} category="unlockable"/>
             <br/>
             <QuestList store={store} category="gold"/>
+          </div>
+        </div>
+        <br/>
+        </div>
+        <div class="ui inverted vertical footer segment">
+          <div class="ui container">
+            <div class="three wide column">
+              <h4 class="ui inverted header">About</h4>
+              <div class="ui inverted link list">
+                <a href="https://github.com/pietervanh">My Github Profile</a>
+              </div>
+            </div>            
+            <div class="seven wide column">
+              <h4 class="ui inverted header">Copyright</h4>
+              <p>Some Assets are linked from <a href="https://habitica.com">HabitRPG</a> which are licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">CC-BY-NC-SA 3.0</a></p>
+            </div>
           </div>
         </div>
       </div>
