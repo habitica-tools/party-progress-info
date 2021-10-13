100807ada3d9941a19d39b89b7ff5165628a017d
DAlgma <dianx93@gmail.com>
2021-10-14 01:01:12 +0300

    Fix UserId disappearing from Add Party modal bug

--- src/Components/AddPartyModal.js
+++ src/Components/AddPartyModal.js
@@ -4,11 +4,17 @@ import { observable, action } from 'mobx';
 
 @observer
 class AddPartyModal extends Component {
-	@observable userId="";
-	@observable key="";
+
+    constructor() {
+        super();
+        this.state = { userId: "", key: "" }
+    }
+
+    componentDidMount() {
+        this.setState({ userId: this.props.userId });
+    }
 
     render({store, userId}) {
-        this.populateUserId(userId);
         return (
         <div class="ui mini modal active">
             <div class="header">Add Party
@@ -20,13 +26,13 @@ class AddPartyModal extends Component {
                 <label>User ID: </label>
                 <input
                     className="user-id"
-                    autoFocus={true} value={this.userId} onChange={this.onUserIdChange}
+                    autoFocus={true} value={this.state.userId} onChange={this.onUserIdChange}
                 /><p/>
                 <label>API Token: </label>
                 <input
                     className="user-key"
                     type="password"
-                    autoFocus={true} value={this.key} onChange={this.onKeyChange}
+                    autoFocus={true} value={this.state.key} onChange={this.onKeyChange}
                 />
                 <p/>
                 <div class="ui blue button" onClick={this.AddParty}><i class="users icon"></i> Add</div>
@@ -45,21 +51,20 @@ class AddPartyModal extends Component {
     }  
 
 	@action populateUserId = (userId) => {
-		this.userId = userId;
+        this.setState({ userId: userId });
 	}
 
 	@action onUserIdChange = (e) => {
-		this.userId = e.target.value;
+        this.setState({ userId: e.target.value });
 	}
 
 	@action onKeyChange = (e) => {
-		this.key = e.target.value;
+        this.setState({ key: e.target.value });
 	}
 
 	@action AddParty = () => {
-		this.props.store.addParty(this.userId, this.key);
-		this.userId = "";
-        this.key = "";
+		this.props.store.addParty(this.state.userId, this.state.key);
+        this.setState({ userId: "", key: "" });
         this.emptyUserIdInParent();
         this.Close();
     }
