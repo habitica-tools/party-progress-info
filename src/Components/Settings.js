272767a74d86fc55318d42ae6fe986cdffdd5a24
DAlgma <dianx93@gmail.com>
2021-10-26 00:46:42 +0300

    #12 Add party size and total pending damage

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -11,14 +11,27 @@ class Settings extends Component {
             store.loadingobjects ? <div class="ui active centered inline loader"></div> :
             <div>
                 <SettingsInput store={store} /><br/>
+                <div class="ui fluid container">            
+                    <div class="ui horizontal divider header">
+                        <h5>
+                            <span data-tooltip="Users" style="color:#0082E2"><i class="user icon"></i>
+                                {store.users.length}
+                            </span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <span data-tooltip="Total pending damage" style="color:#646464"><i class="bomb icon"></i>
+                                {store.users.map(u => u.loading ? 0 : u.damage).reduce((a, b) => a + b, 0)}
+                            </span>
+                        </h5>
+                    </div>      
+                </div>
+                <br/>
                 <div class="ui cards">
                     {store.users.map(u => u.loading ? <div class="ui active centered inline loader"></div> : <User user={u} /> )}
                 </div>
                 <br/>
             </div>
-            );
+        );
     }
-
 
 }
 
