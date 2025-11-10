537c21be4e77923fe878425b11c27148730bbb23
Turac Ramotar <turac.ramotar@gmail.com>
2025-11-10 14:59:12 +0100

    Bump babel version

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 
     //input
-    entry: ['babel-polyfill', './src'],
+    entry: ['./src'],
 
     //output
     output: {
