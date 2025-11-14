2e8e8b00a30f701f0d8339415bbc791b20c850b2
Turac Ramotar <turac.ramotar@gmail.com>
2025-11-14 12:19:14 +0100

    Include styling in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,32 @@
-import configPreact from "eslint-config-preact";
+import stylistic from "@stylistic/eslint-plugin";
+import preactConfig from "eslint-config-preact";
+import { defineConfig } from "eslint/config";
 
-export default [
-  ...configPreact,
-];
+export default defineConfig([
+    {
+        extends: [preactConfig],
+    },
+    {
+        ignores: [
+            "dist/**",
+            "node_modules/**",
+        ],
+    },
+    {
+        plugins: {
+            "@stylistic": stylistic,
+        },
+    },
+    {
+        files: ["./*.js"],
+        rules: {
+            "@stylistic/indent": ["error", 4],
+        }
+    },
+    {
+        files: ["./src/**/*.js"],
+        rules: {
+            "@stylistic/indent": ["error", 2],
+        }
+    }
+])
