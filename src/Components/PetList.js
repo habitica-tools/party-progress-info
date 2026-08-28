703a39b1f125f77c001be84bec4fbcf5ba5d600c
Turac Ramotar <turac.ramotar@gmail.com>
2026-08-28 15:22:25 +0200

    Create PetLeaderBoard

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,21 +1,13 @@
 import { Component } from 'preact';
 
-import { action, observable } from 'mobx';
 import { observer } from 'mobx-react';
 
 import CombinedPetList from './Lists/CombinedPetList';
+import PetLeaderBoard from './PetLeaderBoard';
 import PetProgressBar from './PetProgressBar';
 
 @observer
 class PetList extends Component {
-  @observable accessor showleaderboard = 'top3';
-  store = null;
-
-  constructor(props) {
-    super(props);
-    this.store = this.props.store;
-  }
-
   render() {
     const { store } = this.props;
 
@@ -27,77 +19,9 @@ class PetList extends Component {
       <div>
         <PetProgressBar store={store} category="quest" />
         <CombinedPetList store={store} category="quest" filterable={false} />
-        <div class="column">
-          <div class="ui horizontal divider header">
-            <h4>Quest Pet Leaderboard</h4>
-          </div>
-          <table class="ui celled table">
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>User</th>
-                <th>Pet Count</th>
-                <th>Percentage of Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.showleaderboard === 'top3' &&
-                store.top3petleaderboard.filter((u) => !u.invalid).map((user, index) => (
-                  user.data.profile !== undefined ? (
-                    <tr key={user.id}>
-                      <td>{index + 1}</td>
-                      <td>{user.data.profile.name}</td>
-                      <td>{user.totalPetCount}</td>
-                      <td>{store.totalCountPetsParty > 0 ? parseFloat(user.totalPetCount / (store.totalCountPets / 100)).toFixed(2) + '%' : '0%'}</td>
-                    </tr>
-                  ) : (
-                    <tr key={user.id}>
-                      <td>{index + 1}</td>
-                      <td>{user.id}</td>
-                      <td>{user.totalPetCount}</td>
-                      <td>{store.totalCountPetsParty > 0 ? parseFloat(user.totalPetCount / (store.totalCountPets / 100)).toFixed(2) + '%' : '0%'}</td>
-                    </tr>
-                  )
-                ))
-              }
-              {this.showleaderboard === 'all' &&
-                store.petleaderboard.filter((u) => !u.invalid).map((user, index) => (
-                  user.data.profile !== undefined ? (
-                    <tr key={user.id}>
-                      <td>{index + 1}</td>
-                      <td>{user.data.profile.name}</td>
-                      <td>{user.totalPetCount}</td>
-                      <td>{store.totalCountPetsParty > 0 ? parseFloat(user.totalPetCount / (store.totalCountPets / 100)).toFixed(2) + '%' : '0%'}</td>
-                    </tr>
-                  ) : (
-                    <tr key={user.id}>
-                      <td>{index + 1}</td>
-                      <td>{user.id}</td>
-                      <td>{user.totalPetCount}</td>
-                      <td>{store.totalCountPetsParty > 0 ? parseFloat(user.totalPetCount / (store.totalCountPets / 100)).toFixed(2) + '%' : '0%'}</td>
-                    </tr>
-                  )
-                ))
-              }
-            </tbody>
-          </table>
-          {this.showleaderboard === 'top3' &&
-            <button class="ui blue button" onClick={this.handleLeaderboardShowAll}><i class="unhide icon" />Show All</button>
-          }
-          {this.showleaderboard === 'all' &&
-            <button class="ui olive button" onClick={this.handleLeaderboardTop3Only}><i class="hide icon" />Top 3 Only</button>
-          }
-        </div>
+        <PetLeaderBoard store={store} category="quest" />
       </div>
     );
-  }
-
-  @action handleLeaderboardShowAll = (e) => {
-    this.showleaderboard = 'all';
-  }
-
-  @action handleLeaderboardTop3Only = (e) => {
-    this.showleaderboard = 'top3';
   }
 }
 
